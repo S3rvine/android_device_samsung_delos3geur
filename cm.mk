9eb75e5d7bf5292c5780c0e03000687500e954a8
Arnav Gosain <nolinuxnoparty@gmail.com>
2013-10-09 16:15:24 +0530

    delos-common -> delos3geur

---
+++ cm.mk
@@ -0,0 +1,26 @@
+# Correct bootanimation size for the screen
+TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 800
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/delos3gxx/full_delos3geur.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Setup device configuration
+PRODUCT_NAME := cm_delos3geur
+PRODUCT_DEVICE := delos3geur
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy Grand Quattro
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_RELEASE_NAME := delos3gxx
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=u8860 \
+	BUILD_FINGERPRINT=samsung/delos3gxx:4.1.2:userdebug/test-keys \
+	PRIVATE_BUILD_DESC="samsung-user 4.1.2 test-keys" \
